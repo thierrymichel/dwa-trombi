87a255243bb2066f10aa34b224852d0d4cd47656
mjosephine <mjosephine_cuvelier@hotmail.com>
2020-10-11 17:38:09 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="app">
+  <div class="app" :class="{ 'open' : isBurgerActive }">
     <div class="nav">
-      <ol>
-        <li><router-link to="/">Data</router-link></li>
-        <li><router-link to="/ref">Ref</router-link></li>
-        <li><router-link to="/reactive">Reactive</router-link></li>
-        <li><router-link to="/computed">Computed</router-link></li>
-        <li><router-link to="/event">Event</router-link></li>
-      </ol>
+      <button class="btn" @click.prevent="toggle">menu</button>
+        <ol>
+          <li><router-link to="/">Data</router-link></li>
+          <li><router-link to="/ref">Ref</router-link></li>
+          <li><router-link to="/reactive">Reactive</router-link></li>
+          <li><router-link to="/computed">Computed</router-link></li>
+          <li><router-link to="/event">Event</router-link></li>
+        </ol>
     </div>
     <router-view class="main" />
+
     <p>- - -</p>
   </div>
 </template>
@@ -27,14 +29,52 @@ body {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
+.open{
+  .nav{
+    ol{
+      transform: translateX(0%);
+    }
+    .btn{
+      &::before{
+        transform: rotate(-45deg) translateY(13px);
+      }
+      &::after{
+        transform: rotate(45deg) translateY(-13px);
+      }
+    }
+  }
+}
 .nav {
   padding: 1rem 2rem;
   border-right: 1px solid gray;
+  
+  .btn{
+    background-color: transparent;
+    border: none;
+    position: fixed;
+    color: white;
+    height: 20px;
+    width: 35px;
+    padding: 0;
+    &::before,
+    &::after{
+      background-color: #42b983;
+      height: 2px;
+      width: 35px;
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    &::after{
+      top: 100%;
+    }
+  }
 
   ol {
-    margin: 0;
+    margin: 60px 0 0;
     padding: 0;
+    transform: translateX(-200%);
   }
 
   a {
@@ -51,3 +91,24 @@ body {
   padding: 5rem;
 }
 </style>
+<script>
+    export default {
+        data: () => ({
+            isBurgerActive: false
+        }),
+        methods: {
+            toggle() {
+                this.isBurgerActive = !this.isBurgerActive
+            }
+        }
+    }
+/*let btn = document.querySelector(".btn");
+let appclass = document.querySelector(".app");
+btn.addEventListener("click", e => {
+  if(appclass.classList.contains("open")){
+    appclass.classList.remove("open")
+  }else{
+    appclass.classList.add("open")
+  }
+});*/
+</script>
