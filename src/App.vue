fb127d71ca5b8398da4f860836ad6c65d719f4fb
Mary-Line <maryline.brouwer@student.heaj.be>
2020-10-11 17:29:52 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="app">
+  <div class="app" :class="{ 'open' : isBurgerActive }">
     <div class="nav">
+      <button class="btn"
+         @click.prevent="toggle">menu</button>
       <ol>
         <li><router-link to="/">Data</router-link></li>
         <li><router-link to="/ref">Ref</router-link></li>
@@ -13,7 +15,18 @@
     <p>- - -</p>
   </div>
 </template>
-
+<script>
+    export default {
+        data: () => ({
+            isBurgerActive: false
+        }),
+        methods: {
+            toggle() {
+                this.isBurgerActive = !this.isBurgerActive
+            }
+        }
+    }
+</script>
 <style lang="scss">
 body {
   margin: 0;
@@ -27,21 +40,59 @@ body {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+.open{
+  .nav{
+    ol{
+    opacity: 1;
+    }
+    .btn{
+      &::before{
+        transform: rotate(-45deg) translateY(10px);
+      }
+      &::after{
+        transform: rotate(45deg) translateX(0);
+      }
+    }
+  }
+}
 
 .nav {
   padding: 1rem 2rem;
   border-right: 1px solid gray;
 
-  ol {
-    margin: 0;
+  .btn{
+    background-color: transparent;
+    border: none;
+    position: fixed;
+    color:white;
+    width: 35px;
     padding: 0;
+    &::after,
+    &::before{
+      content:'';
+      position: absolute;
+      height: 2px;
+      background-color: red;
+      width: 35px;
+      top: 0;
+      left: 0;
+    }
+    &::after{
+      top: 60%;
+    }
+  }
+
+  ol {
+    margin: 60px 0 0;
+    padding: 0;
+    opacity: 0;
   }
 
   a {
     font-weight: bold;
     color: #2c3e50;
 
-    &.router-link-exact-active {
+    &.router-link-exact-active{
       color: #42b983;
     }
   }
