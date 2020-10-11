9fb2eb302c5aca6a1c9f872b5766c0ed1f0f465d
christopher-grauwen <crewsor@hotmail.be>
2020-10-11 20:38:57 +0200

    Exercice burger menu
    Bonjour

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
         <li><router-link to="/event">Event</router-link></li>
       </ol>
     </div>
+    <button class="bt_navMain" @click="blabla">Menu</button>
     <router-view class="main" />
     <p>- - -</p>
   </div>
@@ -31,6 +32,12 @@ body {
 .nav {
   padding: 1rem 2rem;
   border-right: 1px solid gray;
+  transform: translateX(0%);
+  transition-duration: 1s;
+
+  &-close{
+    transform: translateX(-100%);
+  }
 
   ol {
     margin: 0;
@@ -50,4 +57,37 @@ body {
 .main {
   padding: 5rem;
 }
+
+.bt_navMain {
+  position: relative;
+  background-image: url('assets/menu.svg');
+  background-position: center;
+  background-size: contain;
+  overflow: hidden;
+  height: 50px;
+  width: 50px;
+  text-indent: 100%;
+  background-repeat: no-repeat;
+  padding: 0;
+  border: none;
+  cursor: pointer;
+  background-color: inherit;
+  margin: 1em;
+}
 </style>
+
+<script>
+export default ({
+  name: 'nav',
+  setup() {
+    var burger=false;
+    var blabla = () => {
+      var nav=document.querySelector('.nav')
+      nav.classList.toggle('nav-close')
+    }
+    return {
+      blabla,
+      burger,
+    }
+}})
+</script>
